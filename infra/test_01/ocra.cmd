2f53b419fdddd2578e687b0f2a0fcdacfd738048
Stefano Soffia <stefano.soffia@utrc.utc.com>
2018-11-26 14:13:04 +0100

    test_01 WIP

--- infra/test_01/ocra.cmd
+++ infra/test_01/ocra.cmd
@@ -3,4 +3,8 @@ set ocra_discrete_time;
 ocra_check_consistency -i ./system.oss;
 reset;
 ocra_check_implementation -i ./system.oss -I ./system.smv -c BT_FSM;
+reset;
+ocra_check_refinement -i ./system.oss;
+#reset;
+#ocra_check_composite_impl -i ./system.oss -m ./system.map;
 quit;
